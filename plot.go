0e9e5c4957245c20ab65383def385e5856e00156
Joel N. Johnson <joel.johnson675@gmail.com>
2022-08-18 13:14:38 -0700

    cabs glass slide (all spontaneous smf28) 10 GHz p/pr separation

--- plot.go
+++ plot.go
@@ -285,7 +285,7 @@ func main() {
 
     cabsData := getCABSData(lock, file)
 
-    setsToPlotCABS := []int{0}
+    setsToPlotCABS := []int{0,1}
     plotCABS(setsToPlotCABS, cabsData, label, sample, length)
   }
 
