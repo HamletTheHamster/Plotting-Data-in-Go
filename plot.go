6b528b6f8756cfebbf5c08f12db42c00b3d76c4f
Joel N. Johnson <joel.johnson675@gmail.com>
2022-09-29 12:04:41 -0700

    no binning (more accurate fits)

--- plot.go
+++ plot.go
@@ -101,7 +101,7 @@ func main() {
 
       var asAmps, asLinewidths []float64
 
-      binSets := []int{0,1,2}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+      binSets := []int{}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
       if len(binSets) > 0 {
         binMHz := 10.
         as, s = bin(binSets, as, s, binMHz)
