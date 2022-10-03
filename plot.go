388aa9ed19a7ad3c526e06f7a11e94fdd85807a6
Joel N. Johnson <joel.johnson675@gmail.com>
2022-10-03 09:28:18 -0700

    no binning

--- plot.go
+++ plot.go
@@ -65,7 +65,7 @@ func main() {
       asLabel, sLabel,
     )
 
-    binSets := []int{0,1,2}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    binSets := []int{}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
     if len(binSets) > 0 {
       binMHz := 5.
       as, s = bin(binSets, as, s, binMHz)
