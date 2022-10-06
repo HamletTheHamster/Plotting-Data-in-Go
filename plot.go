3d0dae22534d85a1cd7ee39104a75dc444938a4d
Joel N. Johnson <joel.johnson675@gmail.com>
2022-10-05 20:34:19 -0700

    y-axis adjust

--- plot.go
+++ plot.go
@@ -65,7 +65,7 @@ func main() {
       asLabel, sLabel,
     )
 
-    binSets := []int{}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    binSets := []int{0,1,2,3}  // 0,4,8,12,15 // 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
     if len(binSets) > 0 {
       binMHz := 5.
       as, s = bin(binSets, as, s, binMHz)
@@ -1077,11 +1077,11 @@ func axes(
         return xrange, yrange, xtick, ytick, xtickLabel, ytickLabel, nil
       } else if coolingExperiment == "pump-probe" {
         xrange := []float64{2, 2.5}
-        yrange := []float64{0, 2.5}
+        yrange := []float64{0, 3}
         xtick := []float64{2, 2.05, 2.1, 2.15, 2.2, 2.25, 2.3, 2.35, 2.4, 2.45, 2.5}
-        ytick := []float64{0, 0.5, 1, 1.5, 2, 2.5}
+        ytick := []float64{0, 0.5, 1, 1.5, 2, 2.5, 3}
         xtickLabel := []string{"2", "", "2.1", "", "2.2", "", "2.3", "", "2.4", "", "2.5"}
-        ytickLabel := []string{"0", "", "1", "", "2", ""}
+        ytickLabel := []string{"0", "", "1", "", "2", "", "3"}
 
         return xrange, yrange, xtick, ytick, xtickLabel, ytickLabel, nil
       }
