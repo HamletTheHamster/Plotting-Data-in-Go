a442b618545761df53dd390e40b15cba7cd45cec
Joel N. Johnson <joel.johnson675@gmail.com>
2023-10-30 15:22:44 -0700

    GIFs

--- gif.go
+++ gif.go
@@ -9,14 +9,20 @@ import (
 	"image/png"
 	"log"
 	"os"
-  "fmt"
-  "strconv"
+	"sort"
+	"strconv"
+	"sync"
 )
+
+type frameResult struct {
+	Index   int
+	Palette *image.Paletted
+}
 
 func main() {
 	// List of PNG filenames
 	pngFilenames := []string{}
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 75; i++ {
 		pngFilenames = append(pngFilenames, "plots/2023-Oct-30/gifFrames/CABS"+strconv.Itoa(i)+".png")
 	}
 
@@ -27,13 +33,32 @@ func main() {
 	var frames []*image.Paletted
 	var delays []int
 
-	for _, fname := range pngFilenames {
-    fmt.Printf(".")
-		img := openPNG(fname)
-		palettedImage := image.NewPaletted(img.Bounds(), palette)
-		draw.Draw(palettedImage, img.Bounds(), img, image.Point{}, draw.Over)
-		frames = append(frames, palettedImage)
-		delays = append(delays, 50)
+	// Channel to receive frame results
+	resultCh := make(chan frameResult, len(pngFilenames))
+	var wg sync.WaitGroup
+
+	for index, fname := range pngFilenames {
+		wg.Add(1)
+		go convertToPaletted(index, fname, palette, resultCh, &wg)
+	}
+
+	wg.Wait()
+	close(resultCh)
+
+	// Collect the results in a slice
+	var frameResults []frameResult
+	for result := range resultCh {
+		frameResults = append(frameResults, result)
+	}
+
+	// Sort the frameResults by their index
+	sort.Slice(frameResults, func(i, j int) bool {
+		return frameResults[i].Index < frameResults[j].Index
+	})
+
+	for _, result := range frameResults {
+		frames = append(frames, result.Palette)
+		delays = append(delays, 5)
 	}
 
 	// Save as a GIF
@@ -45,8 +70,19 @@ func main() {
 	})
 }
 
+func convertToPaletted(index int, fname string, palette []color.Color, resultCh chan<- frameResult, wg *sync.WaitGroup) {
+	defer wg.Done()
+	img := openPNG(fname)
+	palettedImage := image.NewPaletted(img.Bounds(), palette)
+	draw.Draw(palettedImage, img.Bounds(), img, image.Point{}, draw.Over)
+	resultCh <- frameResult{
+		Index:   index,
+		Palette: palettedImage,
+	}
+}
+
 func generatePalette(img image.Image) []color.Color {
-	paletted := image.NewPaletted(img.Bounds(), palette.Plan9) // Use Plan9 as a base
+	paletted := image.NewPaletted(img.Bounds(), palette.Plan9)
 	draw.Draw(paletted, img.Bounds(), img, image.Point{}, draw.Over)
 	return paletted.Palette
 }
