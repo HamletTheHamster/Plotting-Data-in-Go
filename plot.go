28a28f00093618797fabaf0c5173eec212179e7a
Joel N. Johnson <joel.johnson675@gmail.com>
2024-09-29 14:46:34 -0700

    pak1chip3-20um4 replication measurement - 5 MHz step and P-Pr sep

--- plot.go
+++ plot.go
@@ -1607,14 +1607,14 @@ func plotCABS(
   }
   legend := ""
 
-  /* Manual Axes
+  // Manual Axes
   xrange, yrange, xticks, yticks, xtickLabels, ytickLabels, err := axes("CABS", sample, "")
   if err != nil {
     fmt.Println(err)
     os.Exit(1)
-  }*/
+  }//
 
-  // Auto Axes
+  /* Auto Axes
   xmax := 0.
   xmin := cabsData[0][0][0]
   for _, set := range sets {
@@ -1701,7 +1701,7 @@ func plotCABS(
     }
   }
   yticks = append(yticks, ymax)
-  ytickLabels = append(ytickLabels, "")//
+  ytickLabels = append(ytickLabels, "")*/
 
   p, t, r := prepPlot(
     title, xlabel, ylabel, legend,
@@ -1934,12 +1934,12 @@ func axes(
 
       return xrange, yrange, xtick, ytick, xtickLabel, ytickLabel, nil
     case "pak1chip3-20um4":
-      xrange := []float64{10.6, 11.3}
-      yrange := []float64{0, 300}
-      xtick := []float64{10.6, 10.65, 10.7, 10.75, 10.8, 10.85, 10.9, 10.95, 11.0, 11.05, 11.1, 11.15, 11.2, 11.25}
-      ytick := []float64{0, 50, 100, 150, 200, 250, 300}
-      xtickLabel := []string{"", "10.65", "", "10.75", "", "10.85", "", "10.95", "", "11.05", "", "11.15", "", "11.25"}
-      ytickLabel := []string{"0", "", "100", "", "200", "", ""}
+      xrange := []float64{10.6, 11.}
+      yrange := []float64{0, 800}
+      xtick := []float64{10.6, 10.65, 10.7, 10.75, 10.8, 10.85, 10.9, 10.95, 11.0}
+      ytick := []float64{0, 150, 300, 450, 600, 750}
+      xtickLabel := []string{"", "10.65", "", "10.75", "", "10.85", "", "10.95", ""}
+      ytickLabel := []string{"0", "", "300", "", "600", ""}
 
       return xrange, yrange, xtick, ytick, xtickLabel, ytickLabel, nil
     case "no-chip":
